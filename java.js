8bd9ab5ec83a0c74fdbe439ad21ac9f61b6ae4e8
Andre Martins <andremartins2803@gmail.com>
2025-11-02 22:09:45 -0300

    teste celular 02

--- java.js
+++ java.js
@@ -79,7 +79,8 @@ function exibir_I() {
         document.getElementById("Direito_tri").style.display = "none";
     }
 }
-var userAgent = navigator.userAgent;
+function inicializar(){
+    var userAgent = navigator.userAgent;
 if (/Mobi|Android|iPhone|iPad|iPod/i.test(userAgent)) {
     document.getElementById("botoes-direitos").style.flexDirection = "column";
     document.getElementById("botoes-index").style.flexDirection = "column";
@@ -87,3 +88,5 @@ if (/Mobi|Android|iPhone|iPad|iPod/i.test(userAgent)) {
     document.getElementById("botoes-direitos").style.flexDirection = "initial";
     document.getElementById("botoes-index").style.flexDirection = "initial";
 }
+}
+inicializar();
