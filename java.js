39061111c7c5633b8fa32fcdeb1ea80804ad7dab
andre martins <andremartins2803@gmail.com>
2025-11-26 19:40:18 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,43 +1,11 @@
-function exibir_G() {
-    if (S_geral.style.display === "none") {
-        document.getElementById("S_geral").style.display = "block";
-        document.getElementById("S_emocional").style.display = "none";
-        document.getElementById("S_dentista").style.display = "none";
-
+function exibirEOcultar(appear,ocult1,ocult2,ocult3) {
+    if (appear.style.display === "none") {
+        appear.style.display = "block";
+        ocult1.style.display = "none";
+        ocult2.style.display = "none";
+        ocult3.style.display = "none";
     } else {
-        document.getElementById("S_geral").style.display = "none";
-    }
-}
-function exibir_E() {
-    if (S_emocional.style.display === "none") {
-        document.getElementById("S_geral").style.display = "none";
-        document.getElementById("S_emocional").style.display = "block";
-        document.getElementById("S_dentista").style.display = "none";
-    } else {
-        document.getElementById("S_emocional").style.display = "none";
-    }
-}
-function exibir_T() {
-    if (T_geral.style.display === "none") {
-        document.getElementById("T_geral").style.display = "block";
-        document.getElementById("T_aposentadoria").style.display = "none";
-    } else {
-        document.getElementById("T_geral").style.display = "none";
-    }
-}
-function exibir_A() {
-    if (T_aposentadoria.style.display === "none") {
-        document.getElementById("T_geral").style.display = "none";
-        document.getElementById("T_aposentadoria").style.display = "block";
-    } else {
-        document.getElementById("T_aposetadoria").style.display = "none";
-    }
-}
-function exibir_Edu() {
-    if (Edu.style.display === "none") {
-        document.getElementById("Edu").style.display = "block";
-    } else {
-        document.getElementById("Edu").style.display = "none";
+        appear.style.display = "none";
     }
 }
 function exibir_QM() {
@@ -47,35 +15,4 @@ function exibir_QM() {
 function exibir_JG() {
     document.getElementById("Qm_somos").style.display = "none";
     document.getElementById("jogo_regras").style.display = "block";
-}
-function Saude() {
-    document.getElementById("saude").style.display = "block";
-    document.getElementById("trabalho").style.display = "none";
-    document.getElementById("Educação").style.display = "none";
-    document.getElementById("impostos").style.display = "none";
-}
-function Trabalho() {
-    document.getElementById("saude").style.display = "none";
-    document.getElementById("trabalho").style.display = "block";
-    document.getElementById("Educação").style.display = "none";
-    document.getElementById("impostos").style.display = "none";
-}
-function educação() {
-    document.getElementById("saude").style.display = "none";
-    document.getElementById("trabalho").style.display = "none";
-    document.getElementById("Educação").style.display = "block";
-    document.getElementById("impostos").style.display = "none";
-}
-function impostos() {
-    document.getElementById("saude").style.display = "none";
-    document.getElementById("trabalho").style.display = "none";
-    document.getElementById("Educação").style.display = "none";
-    document.getElementById("impostos").style.display = "block";
-}
-function exibir_I() {
-    if (Direito_tri.style.display === "none") {
-        document.getElementById("Direito_tri").style.display = "block";
-    } else {
-        document.getElementById("Direito_tri").style.display = "none";
-    }
 }
