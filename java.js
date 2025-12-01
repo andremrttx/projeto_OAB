9b67c41983e13b6c0a866af44b69a6e05dec0feb
andre martins <andremartins2803@gmail.com>
2025-12-01 01:02:18 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,6 +1,18 @@
+function identifyCell(){
+var isCell = false;
+var navegador = navigator.userAgent;
+if(navegador.indexOf('android') != -1){
+    return isCell = true;
+}else if(navegador.indexOf('iPhone') != -1){
+    return isCell = true;
+}else{
+    return isCell;
+}
+}
 function exibirEOcultar(appear,ocult1,ocult2,ocult3) {
     if (appear.style.display === "none") {
         appear.style.display = "block";
+        appear.scrollIntoView();
         ocult1.style.display = "none";
         ocult2.style.display = "none";
         ocult3.style.display = "none";
